49a060be5e0cce7b23fc2af850725668bb5eb989
Yash6352-rs <yashpanchal14222004@gmail.com>
2025-06-20 17:26:23 +0530

    Done

--- server/server.js
+++ server/server.js
@@ -30,7 +30,7 @@ app.get("/", (req, res) => {
 });
 
 // Connect to DB and start server
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 10000;
 mongoose.connect(process.env.MONGO_URI, {
   useNewUrlParser: true,
   useUnifiedTopology: true,
